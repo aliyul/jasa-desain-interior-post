483403572549c4fff784c3b003ab15c376122eb6
aliyul <waliprogrammer@gmail.com>
2025-07-26 21:29:44 +0700

    Update jasa-desain-interior-post.js

--- jasa-desain-interior-post.js
+++ jasa-desain-interior-post.js
@@ -123,4 +123,9 @@ if (urlMappingJasaDesainInterior[cleanUrlJasaDesInPost]) {
        // JasaBorAirPostLink.style.visibility = 'visible';
         pageNameJasaDesInPost.textContent = urlMappingJasaDesainInterior[cleanUrlJasaDesInPost];
     }
+	   // ✅ Tambahkan logika penyesuaian judul halaman ke elemen ID tertentu
+  const title = document.title.replace(" - beton jaya readymix", "").trim();
+  const targets = document.querySelectorAll(".judulHalamanOtomatis");
+  targets.forEach(el => el.textContent = title);
+
    });
